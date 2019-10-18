7a0bcb224f67d68c38748c79d0ea97c058f11b9e
Robyn Wang <tyrobynwang@gmail.com>
2019-10-18 20:23:33 +0000

    Gram 13

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#update action" do
+    it "should allow users to successfully update grams" do
+      gram = FactoryBot.create(:gram, message: "Initial Value")
+      patch :update, params: { id: gram.id, gram: { message: 'Changed' } }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "Changed"
+    end
+
+    it "should have http 404 error if the gram cannot be found" do 
+      patch :update, params: { id: "YOLOSWAG", gram: { message: 'Changed' } }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable_entity" do
+      gram = FactoryBot.create(:gram, message: "Initial Value")
+      patch :update, params: { id: gram.id, gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial Value"
+    end
+  end
+
   describe "gram#edit action" do
     it "should successfully show the edit form if the gram is found" do
       gram = FactoryBot.create(:gram)
@@ -41,7 +64,6 @@ RSpec.describe GramsController, type: :controller do
     it "should require users to be logged in" do
       get :new
       expect(response).to redirect_to new_user_session_path
-
     end
 
     it "should successfully show the new form" do

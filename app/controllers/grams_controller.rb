61ff169240d364b7d3d7897dc0dbf021bf72080e
Robyn Wang <tyrobynwang@gmail.com>
2019-10-18 05:15:50 +0000

    Gram 12

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,8 +1,19 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+
   def new
     @gram = Gram.new
+  end
+
+  def index
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   def create
@@ -13,9 +24,6 @@ class GramsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end 
-
-  def index
-  end
 
   private
 

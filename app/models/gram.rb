946ed8c00ff4688b97f831258dbeb257c2f5b59a
Robyn Wang <tyrobynwang@gmail.com>
2019-10-21 05:33:29 +0000

    Gram 17

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,8 @@
 class Gram < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+  
   validates :message, presence: true
+  validates :picture, presence: true
 
   belongs_to :user
 end
